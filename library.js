d8d5357a3af6b16c5c8b9a83ee54a3f8f8d5091f
Tomas Kafka <tk@tomaskafka.com>
2015-08-17 12:56:46 +0200

    properly handles frame of linkto: inside layer group!

--- library.js
+++ library.js
@@ -29,3 +29,61 @@ function showOrHideLinkLayers (shouldShowLayers) {
   }
   return shouldShowLayers;
 }
+
+function showAlert(title, msg) {
+  var app = [NSApplication sharedApplication];
+  [app displayDialog:msg withTitle:title]
+}
+
+function _debug() {
+  return;
+  var args = Array.prototype.slice.call(arguments);
+  showAlert('', args.map(function(a) { return JSON.stringify(a) }).join(', '));
+}
+
+function getLayerClassName(layer) {
+  if (layer instanceof MSPage) return 'MSPage';
+  if (layer instanceof MSArtboardGroup) return 'MSArtboardGroup';
+  if (layer instanceof MSLayerGroup) return 'MSLayerGroup';
+  if (layer instanceof MSLayer) return 'MSLayer';
+  return null;
+}
+
+function getLayerFrame(layer) {
+  f = null;
+  if (typeof layer.frame === 'function') {
+    f = {
+      x: layer.frame().x(),
+      y: layer.frame().y(),
+      w: layer.frame().width(),
+      h: layer.frame().height(),
+    }
+  }
+  return f;
+}
+
+function getLayerAbsoluteFrame(layer) {
+  frame = getLayerFrame(layer);
+  parent = layer;
+
+  _debug('getLayerAbsoluteFrame', layer.name(), 'frame:', frame, 'parentClass:', getLayerClassName(layer.parentGroup()));
+
+  if (frame) {
+    while (parent.parentGroup() && getLayerClassName(parent.parentGroup()) === 'MSLayerGroup') {
+      parent = parent.parentGroup();
+      parentClassName = getLayerClassName(parent);
+      parentFrame = getLayerFrame(parent);
+
+      _debug('getLayerAbsoluteFrame parent:', parentFrame);
+
+      if (parentFrame) {
+        frame.x += parentFrame.x;
+        frame.y += parentFrame.y;
+      }
+    }
+  }
+
+  _debug('getLayerAbsoluteFrame result:', frame);
+
+  return frame;
+}
